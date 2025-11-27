b930c440cebc641f3385913b2ec187df1de6e606
David <davidsatafi@gmail.com>
2025-11-27 13:47:05 +0100

    Añadir archivo de texto (direcciones.txt) y Registro.java

---
+++ src/Registro.java
@@ -0,0 +1,17 @@
+public class Registro {
+    private String tipo;
+    private String valor;
+
+    public Registro(String tipo, String valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getTipo() { return tipo; }
+    public String getValor() { return valor; }
+
+    @Override
+    public String toString() {
+        return tipo + " " + valor;
+    }
+}
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,13 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Servidor {
-    public static void main(String[] args) {
-        Map<String,List<String>> listaDNS = new HashMap<>();
-        List<String> listaDNS1 = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        Map<String, List<Registro>> tablaDNS = new HashMap<>();
 
 
+        final int puerto = 5000;
+        final String host = "localhost";
+        try (ServerSocket servidor = new ServerSocket(puerto)) {
+            System.out.println("ClienteServidorBase.Servidor iniciado. Esperando conexión en el puerto " + puerto + "...");
+
+            // Espera hasta que un cliente se conecte
+            Socket cliente = servidor.accept();
+            System.out.println("ClienteServidorBase.Cliente conectado desde: " + cliente.getInetAddress().getHostAddress());
+
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
+        }
     }
 }
