21901a99fe74f97979eb7f7bd85ee6c83061fd8a
David <davidsatafi@gmail.com>
2025-12-01 18:41:06 +0100

    He creado 2 bucles "infinitos", en los que en el inicial espera a que entre una conexion de cliente, cuando inicia la conexión, se genera otro bucle infinito hasta que se introduce "exit" permitiendo realizar solicitudes hasta ese momento, en cada solicitud se comprueba su formato con una expresion regular, si coincide con el formato esperado, se hace la solicitud a la lista mapeada y dependiendo del resultado de la solicitud dara el tipo de respuesta indicado en el PDF

--- src/Registro.java
+++ src/Registro.java
@@ -1,18 +1,25 @@
 public class Registro {
     private String tipo;
-    private String valor;
+    private String ip;
 
     public Registro(String tipo, String valor) {
         this.tipo = tipo;
-        this.valor = valor;
+        this.ip = valor;
     }
 
     public String getTipo() {
         return tipo;
     }
 
-    public String getValor() {
-        return valor;
+    public String getIp() {
+        return ip;
     }
 
+    @Override
+    public String toString() {
+        return "Registro{" +
+                "tipo='" + tipo + '\'' +
+                ", valor='" + ip + '\'' +
+                '}';
+    }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -11,28 +11,70 @@ public class Servidor {
         Map<String, List<Registro>> registros = new HashMap<>();
 
 
-        File file = new File("registros.txt");
+        File file = new File("direcciones.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String line;
         while ((line = br.readLine()) != null) {
-            String[] lineas = line.split(",");
-            registros.put(lineas[0], new ArrayList<>());
+            String[] partes = line.split("\\s+");
+            String dominio = partes[0];
+            String tipo = partes[1];
+            String ip = partes[2];
+            Registro reg = new Registro(tipo, ip);
+
+            registros.computeIfAbsent(dominio, k -> new ArrayList<>()).add(reg);
+
         }
-
-
+        // SOLICITUD AL MAPEO CON CLAVE
+       /* List<Registro> solicitud = registros.get("google.com");
+        System.out.println(solicitud.getFirst().getValor());*/
 
         final int puerto = 5000;
         final String host = "localhost";
         try (ServerSocket servidor = new ServerSocket(puerto)) {
-            System.out.println("ClienteServidorBase.Servidor iniciado. Esperando conexión en el puerto " + puerto + "...");
+            System.out.println("Servidor DNS iniciado. Esperando conexión en el puerto " + puerto + "...");
 
-            // Espera hasta que un cliente se conecte
-            Socket cliente = servidor.accept();
-            System.out.println("ClienteServidorBase.Cliente conectado desde: " + cliente.getInetAddress().getHostAddress());
+            while (true) {//vuelta a empezar al cerrar el cliente
+                Socket cliente = servidor.accept();
+                System.out.println("Cliente conectado desde: " + cliente.getInetAddress().getHostAddress());
 
-            BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-            PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+                PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
+                while (true) {
+                    try {
 
+
+                        String regex = "^LOOKUP\\s+(A|CNAME|MX)\\s+([a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})$";
+                        salida.println("Por favor, realiza una solicitud con este formato: (  LOOKUP <tipo> <dominio>  )");
+                        String peticion = entrada.readLine();
+                        if (peticion.equalsIgnoreCase("exit")) {
+                            salida.println("Cerrando conexión...");
+                            cliente.close();
+                            break;
+                        }
+//si concuerda con la regex
+
+                        if (peticion.matches(regex)) {
+                            String[] partes2 = peticion.split("\\s+");
+                            String tipo = partes2[1];
+                            String dominio = partes2[2];
+
+                            List<Registro> solicitud = registros.get(dominio);
+                            //si hay algun registro con la clave solicitada
+                            if (solicitud != null && solicitud.getFirst().getTipo().matches(tipo)) {
+                                salida.println("200 " + solicitud.getFirst().getIp());
+                            } else {//si la solicitud es correcta pero la clave no existe
+                                salida.println("404 Not Found");
+                            }
+                        } else {// si la solicitud no concuerda con la regex
+                            salida.println("400 Bad Request.");
+                        }
+
+                    } catch (Exception e) {
+                        salida.println("500 Server Error");
+                    }
+                }
+
+            }
         }
     }
 }
